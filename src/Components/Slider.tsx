138ab5eba562214bae89725b82334f086220418d
DannyJimenezA <anjiar03@hotmail.com>
2025-06-06 19:53:01 -0600

    prueba rendimiento

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -48,12 +48,19 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 // Importa tus imágenes con la ruta correcta
-import image1 from '../Img/img05.jpg';
-import image2 from '../Img/Img02.jpg';
-import image3 from '../Img/Img03.jpg';
+// import image1 from '../Img/img05.jpg';
+// import image2 from '../Img/Img02.jpg';
+// import image3 from '../Img/Img03.jpg';
 
 // Asigna las imágenes importadas al array
-const images = [image1, image2, image3];
+// const images = [image1, image2, image3];
+const images = [
+  "https://res.cloudinary.com/dxztlnzdp/image/upload/f_auto,q_auto,w_1280,c_fill/v1749261000/img05_ns145p.jpg",
+  "https://res.cloudinary.com/dxztlnzdp/image/upload/f_auto,q_auto,w_1280,c_fill/v1749261000/Img02_ajyyrw.jpg",
+  "https://res.cloudinary.com/dxztlnzdp/image/upload/f_auto,q_auto,w_1280,c_fill/v1749261000/Img03_q5xpcr.jpg",
+];
+
+
 
 export default function ImageSlider() {
   const settings = {
@@ -71,11 +78,21 @@ export default function ImageSlider() {
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index} className="outline-none">
-            <img src={image} alt={`Slide ${index + 1}`} className="w-full h-[400px] object-cover" />
+            <div className="aspect-video overflow-hidden rounded-lg shadow-md">
+              <img
+                src={image}
+                alt={`Slide ${index + 1}`}
+                loading={index === 0 ? "eager" : "lazy"}
+                width={1280}
+                height={720}
+                className="w-full h-full object-cover object-center"
+              />
+            </div>
           </div>
         ))}
       </Slider>
     </div>
   );
 }
+
 
